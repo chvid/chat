d117e519102a0e7c7901d4faf8fdb615a8d72595
Christian Hvid <christian.hvid@gmail.com>
2020-12-29 13:49:46 +0100

    Proper wrapping of websocket object.

--- client/src/Main.js
+++ client/src/Main.js
@@ -12,19 +12,28 @@ export class Main extends React.Component {
             messages: []
         };
         this.wsServer = new WsServer();
-        this.wsServer.on("newChatLine", m => {
+        this.wsServer.onMessage("newChatLine", m => {
             this.setState({ messages: [...this.state.messages.slice(Math.max(0, this.state.messages.length - 10)), m] });
             window.scrollTo(0, document.body.scrollHeight);
         });
-        this.wsServer.on("ping", m => {
+        this.wsServer.onMessage("ping", m => {
             // this.setState({ messages: [... this.state.messages.slice(Math.max(0, this.state.messages.length - 10)), "ping: " + m] });
         });
-        this.wsServer.onInit(() => {
+        this.wsServer.onOpen(() => {
+            this.setState({ status: "running"});
             this.wsServer.send("post", {
                 username: this.username,
                 date: new Date().getTime(),
                 message: "Hello"
             });
+        });
+        this.wsServer.onError((e) => {
+            console.log("onError", e);
+            this.setState({ status: "error: " +e });
+        });
+        this.wsServer.onClose((e) => {
+            console.log("onClose", e);
+            this.setState({ status: "close: " +e });
         });
     }
 
@@ -44,10 +53,16 @@ export class Main extends React.Component {
     render() {
         return (
             <div>
+                {
+                    this.state.status != "running" &&
+                    <div className="overlay">
+                        <div className="dialog">{this.state.status}</div>
+                    </div>
+                }
                 <div className="main">
                     <div className="lines">
-                        {this.state.messages.map(m => (
-                            <div className="line">
+                        {this.state.messages.map((m, i) => (
+                            <div className="line" key={i}>
                                 <p className="message">{m.message}</p>
                                 <p className="signature">
                                     {m.username} - {formatTime(m.date)}
--- client/src/hsws.js
+++ client/src/hsws.js
@@ -1,39 +1,57 @@
 export class WsServer {
     constructor() {
-        this.handlers = {};
-        this.initHandlers = [];
+        this.messageHandlers = {};
+        this.openHandlers = [];
+        this.closeHandlers = [];
+        this.errorHandlers = [];
     }
 
     init() {
         this.socket = new WebSocket((window.location.protocol == "https:" ? "wss:" : "ws:") + window.location.host + "/ws");
 
         this.socket.onopen = () => {
-            for (let h of this.initHandlers) {
+            for (let h of this.openHandlers) {
                 h();
             }
         };
 
         this.socket.onmessage = event => {
             const envelope = JSON.parse(event.data);
-            if (this.handlers[envelope.header]) {
-                for (let h of this.handlers[envelope.header]) {
+            if (this.messageHandlers[envelope.header]) {
+                for (let h of this.messageHandlers[envelope.header]) {
                     h(envelope.body);
                 }
             }
         };
 
         this.socket.onerror = event => {
-            console.log("Error from server ", event.data);
+            for (let h of this.errorHandlers) {
+                h(event.message);
+            }
         };
+
+        this.socket.onclose = event => {
+            for (let h of this.closeHandlers) {
+                h(event.reason);
+            }
+        }
     }
 
-    onInit(handler) {
-        this.initHandlers.push(handler);
+    onOpen(handler) {
+        this.openHandlers.push(handler);
     }
 
-    on(header, handler) {
-        if (this.handlers[header] == null) this.handlers[header] = [];
-        this.handlers[header].push(handler);
+    onClose(handler) {
+        this.closeHandlers.push(handler);
+    }
+
+    onError(handler) {
+        this.errorHandlers.push(handler);
+    }
+
+    onMessage(header, handler) {
+        if (this.messageHandlers[header] == null) this.messageHandlers[header] = [];
+        this.messageHandlers[header].push(handler);
     }
 
     send(header, body) {
